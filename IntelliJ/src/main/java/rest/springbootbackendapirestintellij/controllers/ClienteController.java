4ff3789ff65c13f9679e092c29a8f8b604301702
Jean Prouvay <jean.prouvay@gmail.com>
2020-09-20 21:41:22 -0300

    implementacion crud cliente

--- IntelliJ/src/main/java/rest/springbootbackendapirestintellij/controllers/ClienteController.java
+++ IntelliJ/src/main/java/rest/springbootbackendapirestintellij/controllers/ClienteController.java
@@ -1,10 +1,12 @@
 package rest.springbootbackendapirestintellij.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import rest.springbootbackendapirestintellij.models.entity.Cliente;
 import rest.springbootbackendapirestintellij.models.services.ClienteService;
 
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8100"})
@@ -18,5 +20,33 @@ public class ClienteController {
     @GetMapping("/clientes")
     public List<Cliente> index(){
         return clienteService.findAll();
+    }
+
+    @GetMapping("/clientes/{id}")
+    public Cliente show(@PathVariable Long id){
+        return clienteService.findById(id);
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente){
+        //cliente.setCreateAt(new Date());
+        return clienteService.save(cliente);
+    }
+
+    @PutMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id){
+        Cliente clienteActual = clienteService.findById(id);
+        clienteActual.setApellido(cliente.getApellido());
+        clienteActual.setNombre(cliente.getNombre());
+        clienteActual.setEmail(cliente.getEmail());
+        return clienteService.save(clienteActual);
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        clienteService.delete(id);
     }
 }
